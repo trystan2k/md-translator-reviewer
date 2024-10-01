de5a5d7025f4eab933378a0f837d552f6642cecb
Thiago Mendonca <trystan2k@gmail.com>
2024-10-02 01:35:05 +0200

    test(*): remove collectCoverageFrom setting

--- jest.config.ts
+++ jest.config.ts
@@ -7,9 +7,6 @@ const config: Config = {
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
 
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['./src/**'],
-
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
